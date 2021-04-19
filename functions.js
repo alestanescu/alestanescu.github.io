39347743753a04ada82789f3a169ae5e691de914
alestanescu <ale.stanescu21@yahoo.com>
2021-04-20 01:47:21 +0300

    first cv version

---
+++ functions.js
@@ -0,0 +1,13 @@
+function showLanguagesPage() {
+    var home = document.getElementById('home');
+    home.style.display = 'none';
+    var page = document.getElementById('languages');
+    page.style.display = '' ;
+}
+
+function showSkillsPage() {
+    var languages = document.getElementById('languages');
+    languages.style.display = 'none';
+    var page = document.getElementById('skills');
+    page.style.display = '';
+}
