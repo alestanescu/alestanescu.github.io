55e93231cda1f3137c22e93ccd2fa58df3545387
alestanescu <ale.stanescu21@yahoo.com>
2021-05-05 01:14:37 +0300

    listenMenuClicks from js

--- functions.js
+++ functions.js
@@ -9,8 +9,10 @@ function show(id) {
 function hideAllPages() {
     var pages = document.querySelectorAll(".page");
     
-    for(var i = 0; i < pagesIds.length; i++) {
-        hide(pages[i].id);
+    for(var i = 0; i < pages.length; i++) {
+       var page = pages[i];
+       hide(page.id);
+       // hide(pages[i].id);
     }
 }
 
@@ -18,3 +20,16 @@ function showPage(pageId) {
     hideAllPages();
     show(pageId);
 }
+
+function listenMenuClicks() {
+    var menu = document.getElementById('top-menu-bar');
+    menu.addEventListener("click", function(e){
+        var link = e.target;
+        if (link.matches("a")) {
+            var id = link.innerHTML.toLowerCase();
+            showPage(id);
+        }
+    });
+}
+
+listenMenuClicks();
